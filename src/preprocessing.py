3750ba3ad2098ce9fe7e029319bc1acc78bd33c8
Muhammad Shafan <muhammadshafan2004@gmail.com>
2026-08-06 15:37:38 +0500

    feat: enhance preprocessing with column renaming and text features
    - Rename v1/v2 to label/message and drop extra columns
    - Add character and word count features for analysis
    - Print shape, label distribution, and summary stats

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,30 @@
 import pandas as pd
 
-df=pd.read_csv("data/raw/spam.csv",encoding='latin-1')
+df=pd.read_csv("data/raw/spam.csv",encoding='latin-1')   #As dataset contains some special characters it would throw UnicodeDecodeError
 print(df.head())
+
+#Rename columns
+df=df.rename(
+    columns={
+        "v1":"label",
+        "v2":"message"
+    }
+)
+
+df=df.loc[:,["label","message"]]
+
+print("Shape:",df.shape)
+
+print(df["label"].value_counts())
+
+#character count
+df['character_count']=df['message'].str.len()
+
+#word count
+df['word_count']=df['message'].str.split().str.len()
+
+#Summary
+print(df["character_count"].describe())
+print(df["word_count"].describe())
+
 
