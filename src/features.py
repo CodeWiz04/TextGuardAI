eee9d268f414e21188d8767864b647bd7ef293d3
Muhammad Shafan <muhammadshafan2004@gmail.com>
2026-08-07 17:03:20 +0500

    feat: add TF-IDF vectorization for train and test sets
    Transform raw text into TF-IDF features using fit_transform on the training set and transform on the test set, ensuring the test data is encoded with the vocabulary learned from training only.

--- src/features.py
+++ src/features.py
@@ -11,3 +11,9 @@ tfidf=TfidfVectorizer(max_features=5000)     #Collects unique words, find count 
                                              #but bag of words isn't enough as all the words get same importance, so we use tfidf which gives weightage to words based on their importance
                                              #Tf=>how important is this word inside the document(Number of times word appears/Total words in document)
                                              #idf=>gives more importance to rare words(log(Total Documents/Number of documents containing the word))
+                                             #max_features=>only consider top 5000 words based on their importance   reduce memory usage,speedup
+
+
+X_train_tfidf=tfidf.fit_transform(X_train)   #learns the vocab,compute idf scores,converts message into tfidf vector
+X_test_tfidf=tfidf.transform(X_test)        #converts message into tfidf vector using the vocab learned from training data  
+
