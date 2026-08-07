56d69c7f53294d13758136f2227f111833730652
Muhammad Shafan <muhammadshafan2004@gmail.com>
2026-08-07 16:51:08 +0500

    fix: correct variable name typo in features.py
    Fix misspelled `y_t,est` to `y_test` in train_test_split and add comments explaining TF-IDF weighting.

--- src/features.py
+++ src/features.py
@@ -5,8 +5,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 X=df['clean_message']
 y=df['label']
 
-X_train,X_test,y_train,y_t,est=train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
 
 tfidf=TfidfVectorizer(max_features=5000)     #Collects unique words, find count occurences of each word,form it into vector called bag of words
                                              #but bag of words isn't enough as all the words get same importance, so we use tfidf which gives weightage to words based on their importance
-
+                                             #Tf=>how important is this word inside the document(Number of times word appears/Total words in document)
+                                             #idf=>gives more importance to rare words(log(Total Documents/Number of documents containing the word))
