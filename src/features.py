afd200e358849e07a0e9a1bef0398f687b447af0
Muhammad Shafan <muhammadshafan2004@gmail.com>
2026-08-07 16:17:29 +0500

    feat: add train-test split and TF-IDF vectorization
    Split data into training and testing sets with stratification, and apply TF-IDF vectorization with max_features=5000 to convert text into weighted feature vectors.

--- src/features.py
+++ src/features.py
@@ -5,4 +5,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 X=df['clean_message']
 y=df['label']
 
+X_train,X_test,y_train,y_t,est=train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+
+tfidf=TfidfVectorizer(max_features=5000)     #Collects unique words, find count occurences of each word,form it into vector called bag of words
+                                             #but bag of words isn't enough as all the words get same importance, so we use tfidf which gives weightage to words based on their importance
 
